1a70b5262c20018074d87a194188a6a5a58c1d4e
mily-C <emilysilvabouras@gmail.com>
2026-04-17 11:59:41 -0400

    Revision de error

--- Taller_iujo_01/Taller_iujo_01/Program.cs
+++ Taller_iujo_01/Taller_iujo_01/Program.cs
@@ -7,6 +7,7 @@
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace Taller_iujo_01
 {
@@ -30,6 +31,27 @@ namespace Taller_iujo_01
 			string Nota = partes[3].Trim();
 			
 			Console.WriteLine(string.Format("El id es: {0} del usuario {1} en la evaluacion {2} con la nota {3}", ID, Nombre, Tarea, Nota));
+			
+
+				
+			//Flujo en archivos
+			
+			string Ruta_Raiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datos IUJO");
+			
+			if(!Directory.Exists("Ruta_Raiz"))
+			{
+				Directory.CreateDirectory("Ruta_Raiz");
+				Console.WriteLine("Se ha creado correctamente");
+			}
+			
+			string archivotexto = Path.Combine(Ruta_Raiz, "notas.txt");
+			
+			Console.WriteLine(archivotexto);
+			
+			using(StreamWriter sw = new StreamWriter(archivotexto, true))
+			{
+				sw.WriteLine(string.Format("ID : {0} nota {1} {yyyy-MM-dd HH:mm}", ID, Nota, DateTime.Now));
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
